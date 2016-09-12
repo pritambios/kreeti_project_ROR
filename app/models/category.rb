5511d9b234ff5ba4351e08fa97b9e7bd67597dc1
Rails project <pritamproject2015@gmail.com>
2016-09-12 12:11:27 +0530

    validation added in models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :items, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 3, maximum: 25}
+  validates :name, presence: true, uniqueness: {case_sensitive: true }, length: { minimum: 3, maximum: 25 }
   validates_uniqueness_of :name
 end
